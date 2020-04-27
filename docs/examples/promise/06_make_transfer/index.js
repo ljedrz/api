d873abf9101b5b89ad0c8fe582c1f6b73e918f23
ljedrz <lj@concordium.com>
2020-04-27 18:55:24 +0200

    alter examples/promise/06

--- docs/examples/promise/06_make_transfer/index.js
+++ docs/examples/promise/06_make_transfer/index.js
@@ -6,25 +6,53 @@
 const { ApiPromise } = require('@polkadot/api');
 const { Keyring } = require('@polkadot/keyring');
 
-const BOB = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
+// Dave's account address
+const dave = '5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy';
+
+// Ferdie's account address
+const ferdie = '5CiPPseXPECbkjWCa6MnjNokrgYjMqmKndv2rSnekmSK2DjL';
 
 async function main () {
   // Instantiate the API
-  const api = await ApiPromise.create();
+  const api = await ApiPromise.create({
+    types: {
+      // mapping the actual specified address format
+      Address: 'AccountId',
+      // mapping the lookup
+      LookupSource: 'AccountId'
+    }
+  });
 
-  // Constuct the keying after the API (crypto has an async init)
-  const keyring = new Keyring({ type: 'sr25519' });
+  // import the test keyring (already has dev keys for Alice, Bob, Charlie, Eve & Ferdie)
+  const testKeyring = require('@polkadot/keyring/testing');
+  const keyring = testKeyring.default();
 
-  // Add Alice to our keyring with a hard-deived path (empty phrase, so uses dev)
-  const alice = keyring.addFromUri('//Alice');
+  // Retrieve the sudo key from the chain state
+  const adminId = await api.query.sudo.key();
 
-  // Create a extrinsic, transferring 12345 units to Bob
-  const transfer = api.tx.balances.transfer(BOB, 12345);
+  // Find the actual keypair in the keyring (if this is a changed value, the key
+  // needs to be added to the keyring before - this assumes we have defaults, i.e.
+  // Alice as the key - and this already exists on the test keyring)
+  const adminPair = keyring.getPair(adminId.toString());
 
-  // Sign and send the transaction using our account
-  const hash = await transfer.signAndSend(alice);
+  // The amount to send
+  const amount = 7777777;
 
-  console.log('Transfer sent with hash', hash.toHex());
+  const sudo_infusion = api.tx.balances.setBalance(dave, amount, 0);
+  const sudo_infusion_hash = api.tx.sudo.sudo(sudo_infusion);
+
+  const sudo_transfer = api.tx.balances.forceTransfer(dave, ferdie, amount);
+  const sudo_transfer_hash = api.tx.sudo.sudo(sudo_transfer);
+
+  const something = api.tx.templateModule.doSomething(Math.floor(Date.now() / 1000));
+
+  const txs = [
+    sudo_infusion_hash,
+    sudo_transfer_hash,
+    something
+  ];
+
+  const ret = await api.tx.utility.batch(txs).signAndSend(adminPair);
 }
 
 main().catch(console.error).finally(() => process.exit());
